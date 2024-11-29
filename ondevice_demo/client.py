fa7f6c96ba2a1bc631e1b98483b912575003ceeb
Masao-Someki <masao.someki@gmail.com>
2024-11-29 00:23:37 +0000

    Add onnxruntime inference script

--- ondevice_demo/client.py
+++ ondevice_demo/client.py
@@ -2,10 +2,13 @@ import sounddevice as sd
 import numpy as np
 import requests
 
+import onnxruntime as ort
+
 
 class Constants:
     sample_rate = 16_000
-    window_size = int(sample_rate * 0.5)
+    #window_size = int(sample_rate * 0.5)
+    window_size = 3000
     stride_size = int(sample_rate * 0.05)
     server = "http://localhost:31589/asr"
 
@@ -13,10 +16,12 @@ class Constants:
 audio_buffer = None
 token_buffer = []
 hidden_state = None
+h_in = np.zeros((1, 1, 1024)).astype(np.float32)
+c_in = np.zeros((1, 1, 1024)).astype(np.float32)
 
 
 def audio_callback(indata, frames, time, status):
-    global audio_buffer, token_buffer
+    global audio_buffer, token_buffer, h_in, c_in
 
     # Prepare input audio
     new_audio = indata.flatten()
@@ -24,23 +29,34 @@ def audio_callback(indata, frames, time, status):
         audio_buffer = new_audio.copy()
     else:
         audio_buffer = np.concatenate((audio_buffer, new_audio))[-Constants.window_size:]
+    print(audio_buffer)
 
     if len(audio_buffer) == Constants.window_size:
-        token = inference(audio_buffer)
+        token, h_in, c_in = inference(audio_buffer, h_in, c_in)
         token_buffer.append(token)
         token_buffer = token_buffer[-128:]
-
-        response = requests.post(Constants.server, json={"tokens": token_buffer})
-        print(token_buffer)
-        print(response.json()["text"])
+        print(token)
+        #response = requests.post(Constants.server, json={"tokens": token_buffer})
+        #print(token_buffer)
+        #print(response.json()["text"])
 
 
 def load_model():
-    return "This is a model."
+    ckpt = "convrnn_model.onnx"
+    model = ort.InferenceSession(ckpt)
+    return model
 
-def inference(audio):
-    print(model)
-    return 0
+def inference(audio, h, c):
+    assert len(audio) == 3000
+    output = model.run(None, {
+        'speech': audio.astype(np.float32),
+        'h_in': h,
+        'c_in': c
+    })
+    unit = np.argmax(output[0], axis=-1)
+    h_next = output[1]
+    c_next = output[2]
+    return unit, h_next, c_next
 
 def send(token):
     print("send")

a41f64f3fc83fc8ff2c394477a700d8c408b7278
Masao-Someki <masao.someki@gmail.com>
2024-12-22 18:37:03 -0600

    Add config and following models
    - Frozen SSL with linear layer
    - Trainable SSL and Linear layer
    - SoundStream Encoder

--- egs/streamingDSU/__init__.py
+++ egs/streamingDSU/__init__.py
@@ -2,7 +2,11 @@
 from .dataset.ASRDataset import ASRDataset
 from .dataset.TTSDataset import TTSDataset
 from .dataset.SVSDataset import SVSDataset
+from .dataset.AudioDataset import AudioDataset
 
 # models
 from .models.convrnn import ConvRNN
 from .models.cvrnn_posemb import ConvRNNPosemb
+from .models.ssl_frozen import FrozenSSLWithLinear
+from .models.ssl_trainable import TrainableSSLWithLinear
+from .models.sound_stream import SoundStreamEncoder
